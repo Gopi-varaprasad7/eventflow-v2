1deb57e2924c4df2e0d030d3a91e6d60fd05a377
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-03-16 00:26:03 +0530

    user schema added

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,1 +1,14 @@
-import mongoose = require("mongoose");
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL as string);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("DB connection error", error);
+        process.exit(1);
+    }
+};
